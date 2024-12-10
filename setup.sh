ed829dcda879957e6b04df9d5a194dafa3c5a782
jasperdew <jasper@jasperdewinter.nl>
2024-12-10 15:17:00 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
 mkdir -p ~/.streamlit/
+
 echo "\
 [server]\n\
 headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
+enableWebsocketCompression = false\n\
+enableXsrfProtection = false\n\
 \n\
 " > ~/.streamlit/config.toml
